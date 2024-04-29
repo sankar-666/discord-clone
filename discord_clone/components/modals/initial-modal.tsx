8621d8f8a8699141ffd9bb8a2ba610ceee3fbfb1
sankar <sankusanku001@gmail.com>
2024-04-30 00:36:05 +0100

    initial modal form implimented

--- discord_clone/components/modals/initial-modal.tsx
+++ discord_clone/components/modals/initial-modal.tsx
@@ -12,6 +12,19 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog"
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react";
+
 const formSchema = z.object({
     name: z.string().min(1, {
         message: "Server name is required."
@@ -22,6 +35,12 @@ const formSchema = z.object({
 })
 
 export const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true)
+    }, [])
+
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -33,9 +52,12 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = (values: z.infer<typeof formSchema>) => {
-
+        console.log(values);
+        
     }
 
+    if (!isMounted) return null;
+    
     return ( 
         <Dialog open>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -47,7 +69,47 @@ export const InitialModal = () => {
                         Give your server a personality with a name and an image. You can always change it later.
                     </DialogDescription>
                 </DialogHeader>
-
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                TODO: Image upload
+                            </div>
+                            <FormField 
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel
+                                            className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                                        >
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} 
+                            />
+                        </div>
+                        <DialogFooter
+                            className="bg-gray-100 px-6 py-4"
+                        >   
+                            <Button
+                                disabled={isLoading}
+                                variant="primary"
+                            >
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
      );
